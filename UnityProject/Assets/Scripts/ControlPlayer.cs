2b59922a5adb84e71a971b66f0c00344a0d4fb00
spellmaker <mbts.org@googlemail.com>
2014-01-25 08:35:16 +0100

    Some changes

---
+++ UnityProject/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPlayer : MonoBehaviour {
+
+	public bool facingRight = true;			// For determining which way the player is currently facing.
+
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+
+	public string horAxis = "Horizontal";
+	public string verAxis = "Vertical";
+	public string fire1 = "Fire1";
+	public string fire2 = "Fire2";
+
+
+
+	public float attackspeed = 5f;
+	private float nextFire = 0;
+
+	public float jumpForce = 1000f;
+
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private bool grounded = false;			// Whether or not the player is grounded.
+	private bool jump = false;
+	private bool iscrouching = false;
+
+	private Animator anim;					// Reference to the player's animator component.
+
+	void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
+
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		if(transform.position.x <= 0){
+			horAxis = "Horizontal";
+			verAxis = "Vertical";
+			fire1 = "Fire1";
+			fire2 = "Fire2";
+			facingRight = true;
+		}
+		else{
+			horAxis = "Horizontal_b";
+			verAxis = "Vertical_b";
+			fire1 = "Fire1_b";
+			fire2 = "Fire2_b";
+			facingRight = false;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+		//anim.SetBool("isgrounded", grounded);
+
+
+		float v = Input.GetAxis(verAxis);
+
+		if(Input.GetButtonDown(fire1) && Time.time > nextFire){	//Normal Attack
+			nextFire = Time.time + attackspeed;
+			anim.SetTrigger("normalpunch");
+		}
+		else if(Input.GetButtonDown(fire2) && Time.time > nextFire){	//Heavy Attack
+			nextFire = Time.time + attackspeed;
+			anim.SetTrigger("heavykick");
+		}	
+		//Jumping
+		if (v > 0 && grounded){
+				jump = true;
+		}
+		else if (v < 0){
+			anim.SetBool("crouching", true);
+			iscrouching = true;
+		}
+		else{
+			anim.SetBool("crouching", false);
+			iscrouching = false;
+		}
+	}
+
+	void FixedUpdate(){
+		float h = 0;
+		if(!iscrouching){
+			// Cache the horizontal input.
+			h = Input.GetAxis(horAxis);
+
+			// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+			if(h * rigidbody2D.velocity.x < maxSpeed)
+				// ... add a force to the player.
+				rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		// If the input is moving the player right and the player is facing left...
+		if(h > 0 && !facingRight)
+			// ... flip the player.
+			Flip();
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if(h < 0 && facingRight)
+			// ... flip the player.
+			Flip();
+
+
+		if(jump)
+		{
+			// Add a vertical force to the player.
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+
+			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+			jump = false;
+		}
+	}
+
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log("object entered");
+		Debug.Log(other.tag);
+		//TODO: Enter code for life handling here
+	}
+
+	void TriggerPunch(){
+	}
+
+	void TriggerKick(){
+	}
+}
---
+++ UnityProject/Assets/Scripts/CycleCharacters.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleCharacters : MonoBehaviour {
+
+	public GameObject[] character_faces;
+	public GameObject[] characters;
+
+	public Transform leftposition;
+	public Transform rightposition;
+
+	public Transform leftspawn;
+	public Transform rightspawn;
+
+	public string axis1;
+	public string axis2;
+	public string confirm1;
+	public string confirm2;
+
+	private GameObject left;
+	private GameObject right;
+	private int lindex = 0;
+	private int rindex = 0;
+
+	private float lasta1 = 0;
+	private float lasta2 = 0;
+
+	private bool p1_ready = false;
+	private bool p2_ready = false;
+
+	// Use this for initialization
+	void Start () {
+		left = Instantiate(character_faces[0], leftposition.position, leftposition.rotation) as GameObject;
+		right = Instantiate(character_faces[0], rightposition.position, rightposition.rotation) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//listen for button presses
+		float a1 = Input.GetAxis(axis1);
+		float a2 = Input.GetAxis(axis2);
+
+
+		if(lasta1 == 0 && !p1_ready){
+			if(a1 < 0){
+				Destroy(left);
+				lindex += 1;
+				if(lindex >= character_faces.Length) lindex = 0;
+				left = Instantiate(character_faces[lindex], leftposition.position, leftposition.rotation) as GameObject;
+
+				Debug.Log("player 1 down");
+			}
+			else if(a1 > 0){
+				Destroy(left);
+				lindex -= 1;
+				if(lindex < 0) lindex = character_faces.Length - 1;
+				left = Instantiate(character_faces[lindex], leftposition.position, leftposition.rotation) as GameObject;
+
+				Debug.Log("player 1 up");
+			}
+		}
+		if(lasta2 == 0 && !p2_ready){
+			if(a2 < 0){
+				Destroy(right);
+				rindex += 1;
+				if(rindex >= character_faces.Length) rindex = 0;
+				right = Instantiate(character_faces[rindex], rightposition.position, rightposition.rotation) as GameObject;
+
+				Debug.Log("player 2 down");
+			}
+			else if(a2 > 0){
+				Destroy(right);
+				rindex -= 1;
+				if(rindex < 0) rindex = character_faces.Length - 1;
+				right = Instantiate(character_faces[rindex], rightposition.position, rightposition.rotation) as GameObject;
+
+				Debug.Log("player 2 up");
+			}
+		}
+
+		lasta1 = a1;
+		lasta2 = a2;
+
+		if(Input.GetButton(confirm1)){
+			p1_ready = true;
+		}
+		if(Input.GetButton(confirm2)){
+			p2_ready = true;
+		}
+
+		if(p1_ready && p2_ready){
+			Destroy(gameObject);
+			Destroy(left);
+			Destroy(right);
+			//Destroy(leftposition);
+			//Destroy(rightposition);
+			//Destroy(leftspawn);
+			//Destroy(rightspawn);
+
+			Instantiate(characters[lindex], leftspawn.position, leftspawn.rotation);
+			Instantiate(characters[rindex], rightspawn.position, rightspawn.rotation);
+		}
+	}
+}
